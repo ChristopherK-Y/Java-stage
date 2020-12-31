ad896d9a354652fbcc63aa88d21352867bb10d3d
ChristopherK-Y <2850070304@qq.com>
2020-12-31 21:56:26 +0800

    ClassTwo

--- EveryClass/ClassTwo/TimeTransform.java
+++ EveryClass/ClassTwo/TimeTransform.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class TimeTransform {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int time = scanner.nextInt();
+        int time = scanner.nextInt(); 
         int s = time % 60;
         int m = (time / 60) % 60;
         int h = time / 60 / 60;
