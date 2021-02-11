8cec5ce360350582d73099c80eb7f5edd69838f3
ChristopherK-Y <2850070304@qq.com>
2021-02-11 13:24:09 +0800

    存在重复元素

---
+++ LeetCode/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+package day05;
+
+import java.util.Arrays;
+
+//https://leetcode-cn.com/problems/contains-duplicate/
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
